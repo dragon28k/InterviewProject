b0939e74402d6085fd4acf125c2a78cef430355b
Santhosh Kumar <mail2santhosh28@gmail.com>
2021-08-13 22:18:24 +0530

    Test 2 modified

--- src/com/interview/Test2.java
+++ src/com/interview/Test2.java
@@ -4,8 +4,12 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		
+		String name="santhosh";
+		if(name.equalsIgnoreCase("santhosh")) {
+			System.out.println("Yes");
+		}else
+			System.out.println("No");
 		
-		System.out.println("Test 2");
 
 	}
 
